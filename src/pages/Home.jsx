3ed21e8c6c62859485e5d0bc02428a9e319a63b1
Mariana-Ma <marianama789@gmail.com>
2025-12-06 22:33:38 -0600

    Add headers to homepage, update index.html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,15 +17,20 @@ export default function Home() {
           </h1>
           {/* Top content row */}
           <div className="flex flex-row items-start">
-            {/* Photo */}
-            <div 
-              className="w-64 h-64 rounded-full flex items-center justify-center mt-12 ml-5 text-white text-xl font-bold z-10"
-              style={{
-                backgroundImage: `url(${marianaPicture})`, 
-                backgroundSize: '110%',                 
-                backgroundPosition: 'center',
-                border: "6px solid #9387dc"
-              }}>
+            <div className="flex flex-col items-center ml-5 mt-14">
+              {/* Photo */}
+              <div 
+                className="w-60 h-60 rounded-full flex items-center justify-center text-white text-xl font-bold z-10"
+                style={{
+                  backgroundImage: `url(${marianaPicture})`, 
+                  backgroundSize: '110%',                 
+                  backgroundPosition: 'center',
+                  border: "6px solid #9387dc"
+                }}>
+              </div>
+              <div className="text-center mt-1">
+                <h1 className="text-base font-garet text-gray-700">Mariana Ma | Northwestern University</h1>
+              </div>
             </div>
 
             {/* Text section */}
@@ -42,16 +47,17 @@ export default function Home() {
                   textColors={["black"]}
                 />
               </div>
-
+              
               {/* Static Paragraph */}
               <p className="text-2xl ml-14 mt-10 text-gray-700 font-garet">
                 Computer scientist, analyst, aspiring SWE, and designer (all art on this page made by me!).
               </p>
+
             </div>
           </div>
 
           {/* Buttons row at bottom */}
-          <div className="absolute bottom-14 left-0 w-full flex justify-center gap-8 mt-8 z-10">
+          <div className="absolute bottom-10 left-0 w-full flex justify-center gap-8 mt-8 z-10">
             <Link to="/aboutme">
               <div className="flex flex-col items-center cursor-pointer transform transition-transform duration-300 hover:scale-110">
                 <img
